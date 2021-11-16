40e23029531343fa774a5a288eea704aa23c8c19
Thamsanqa Mbodla <77463814+Thamsanqa-bit@users.noreply.github.com>
2021-11-16 05:21:27 -0800

    Update settings.py

--- ShopNow/settings.py
+++ ShopNow/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE=DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
